011983db24cae349ee466fa4b66d029ea2ea4a97
OhhyunK <ohhyunk@email.wustl.edu>
2022-03-26 15:58:41 -0500

    tony

---
+++ LettuceChoose/src/main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String args[]){    
+
+	
+		Scanner cst = new Scanner(System.in);
+	    System.out.println("Your name please");
+	    System.out.println(cst);
+	    		
+		String name = cst.nextLine();
+
+		int age = cst.nextInt();
+	    double salary = cst.nextDouble();
+	    
+	    System.out.println("Any allergies?");
+	    System.out.println(name);
+
+	    System.out.println(age);
+	    System.out.println(salary);
+
+
+		
+		
+		
+		
+		
+	
+	}
+	
+	
+	
+}
