96d7392a6c00be9864aa5f2372452448307a902f
Jiwoo Seo <jiwooseo@wustl.edu>
2022-03-26 16:13:59 -0500

    updates in main

--- LettuceChoose/src/main.java
+++ LettuceChoose/src/main.java
@@ -4,23 +4,23 @@ public class main {
     public static void main(String args[]){    
 
 	
-		Scanner cst = new Scanner(System.in);
-	    System.out.println("Your name please");
-		String name = cst.nextLine();
-	    System.out.println("Hi" + name);
-	    
-	    System.out.println("Any allergies?");
-		String tk = cst.nextLine();
-	    System.out.println(name);
+    	Scanner cst = new Scanner(System.in);
+    	System.out.println("Welcome to the Lettuce Choose!");
+    	System.out.println("Your name please?");
+    	String name = cst.nextLine();
+    	
+    	System.out.println("Hi, " + name + "! Any allergies?");
+    	String allergies = cst.nextLine();
+    	System.out.println(allergies);
 	    
 	    
 	    		
 
-		int age = cst.nextInt();
-	    double salary = cst.nextDouble();
-	    
-	    System.out.println("Any allergies?");
-	    System.out.println(name);
+//    	int age = cst.nextInt();
+//    	double salary = cst.nextDouble();
+//	    
+//    	System.out.println("Any allergies?");
+//    	System.out.println(name);
 
 
 
