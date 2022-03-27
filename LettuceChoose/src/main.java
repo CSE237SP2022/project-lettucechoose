dab11a473baebaff53b6dce5f47933d6204b95a7
Jiwoo Seo <jiwooseo@wustl.edu>
2022-03-27 16:24:16 -0500

    updates in main.java

--- LettuceChoose/src/main.java
+++ LettuceChoose/src/main.java
@@ -4,11 +4,12 @@ public class main {
 	public static void main(String args[]){    
 
 		Scanner cst = new Scanner(System.in);
-		System.out.println("Your name please");
+		System.out.println("Hi! Welcome to the LettuceChoose!🥗✔️ ");
+		System.out.println("Your name please?");
+		
 		String name = cst.nextLine();
-		System.out.println("Hi" + name);
+		System.out.println("Hi, " + name + ". Do you have any allergies?");
 
-		System.out.println("Any allergies?");
 		String allergies = cst.nextLine();
 		System.out.println(allergies);
 
@@ -16,19 +17,19 @@ public class main {
 		String diet = cst.nextLine();
 		System.out.println(diet);
 
-		System.out.println("Base? Salad, Soba, Brown Rice, White Rice");
+		System.out.println("Choose your base: Salad, Soba, Rice");
 		String base = cst.nextLine(); 
 		System.out.println("Base: " + base);
 
-
-		System.out.println("toppings? edamame, green onioins, tomato, corn, guac");
-		String toppings = cst.nextLine();
-		System.out.println("You may choose up to 3. Otherwise, it will be extra charge. Toppings: " + toppings);
-
-
-		System.out.println("Choose one protein: Beef, Chicken, Salmon, Tuna, Tofu");
+		System.out.println("Choose one protein: Beef, Chicken, Salmon, Tofu");
 		String protein = cst.nextLine(); 
 		System.out.println("protein: " + protein);
+
+		System.out.println("Choose your toppings: edamame, guacamole, tomato, onions, masago");
+		System.out.println("You can choose up to 3 toppings. Otherwise, there will be extra charge.");
+		String toppings = cst.nextLine();
+		System.out.println("Toppings: " + toppings);
+
 
 		System.out.println("Choose drizzle: Roasted Garlic Sauce, Chilli Honey Sauce, Ranch, Ponzu Sauce");
 		String drizzle = cst.nextLine(); 
@@ -37,7 +38,7 @@ public class main {
 
 		System.out.println("Any drinks? Coke, Sprite, water");
 		String drinks = cst.nextLine(); 
-		System.out.println("drizzles: " + drinks);
+		System.out.println("drinks: " + drinks);
 
 
 		System.out.println("How many utensils do you need?");
@@ -59,7 +60,7 @@ public class main {
 		double price;
 		price = 0.0;    
 		System.out.println("Your total order price is: " + price);
-		System.out.println("Thank you for your oder." + name + " Here is" + protein + base + " with " + toppings + "and " + drizzle);
+		System.out.println("Thank you for your order, " + name + "! Here is " + protein + base + " with " + toppings + " and " + drizzle);
 	}
 
 }
