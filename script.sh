2e7b2a861b8b1956cbd2cb071a7d2bb253457f05
Jiwoo Seo <jiwooseo@wustl.edu>
2022-03-29 18:37:45 -0500

    bash script

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-java LettuceChoose/src/main.java
+cd LettuceChoose/src
+
+javac Order.java
+javac Base/Base.java
+javac Protein/Protein.java
+javac Topping/Topping.java
+javac main.java
+
+java main
