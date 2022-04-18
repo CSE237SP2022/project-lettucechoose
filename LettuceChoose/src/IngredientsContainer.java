d224c4707c1527d1dd420aea832bcf31b0c808b8
YJ Yoon <yoon.y@wustl.edu>
2022-04-18 13:38:44 -0500

    Create IngredientsContainer.java

---
+++ LettuceChoose/src/IngredientsContainer.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unchecked")
+public class IngredientsContainer {
+	/**
+	 *  ingredientsInventory - keeps track of the quantity of each base. 
+	 *  integerToIngredient - maps the integer value & string value of base. 
+	 */
+	public Map<String, Integer> ingredientsInventory;
+	private Map<Integer, String> integerToIngredient;
+	
+	public IngredientsContainer(HashMap<String,String>[] ingredientsList){
+		for (HashMap<String,String> nameAndQuantity:ingredientsList) {
+			
+		}
+	}
+	
+	
+	
+	
+	
+	/**
+	 * 
+	 * @param selected - the value that Scanner reads (the user input)
+	 * @return - returns true when the value is in the map, returns false otherwise
+	 * 
+	 * This is to check whether the user input is a valid value for base
+	 */
+	public Boolean doesExists(Integer selected) {
+		return this.integerToIngredient.containsKey(selected);
+	}
+	
+	
+	
+	
+	
+	/**
+	 * 
+	 * @param selectedBase - the value that user picks for the base
+	 * @return - returns true if the selectedBase is in stock. returns false otherwise.
+	 */
+	public Boolean isAvailable(String selectedBase) {
+		int ingredientCount = this.ingredientsInventory.get(selectedBase);
+		
+		if (ingredientCount < 1) {
+			return false;
+		}
+		
+		this.ingredientsInventory.put(selectedBase, ingredientCount-1);
+		return true;
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * @param selectedBase - The value the user types in the command line
+	 * @return - String value of the base. 
+	 * 
+	 * Users are prompted to type integer value of the base.
+	 * This is to find the string value of the base. 
+	 */
+	public String getBaseString(Integer selectedBase) {
+		return this.integerToIngredient.get(selectedBase);
+	}
+
+}
