d10ec9f7cf464c419fcfdd5a489a0b79d5301a5f
Jiwoo Seo <jiwooseo@wustl.edu>
2022-04-20 00:09:53 -0500

    changes in script and some agenda to main

--- LettuceChoose/src/main.java
+++ LettuceChoose/src/main.java
@@ -8,6 +8,11 @@ public class main {
 		Restaurant restaurant = new Restaurant();
 		
 		while(true) {
+			System.out.println("Hi, Welcome to LettuceChoose!");
+			// check if the person is a vendor 
+			// 		if the person is the vendor, s/he will be prompted to other questions
+			
+			// take new order for customers
 			takeNewOrder(restaurant);
 		}
 	}
@@ -15,14 +20,13 @@ public class main {
 	private static void takeNewOrder(Restaurant restaurant) {
 		Scanner scanner = new Scanner(System.in);
 		Order order = new Order();
-		System.out.println("Hi, Welcome to LettuceChoose!");
 		restaurant.askAndSetName(order, scanner);
 		restaurant.askAndSetIngredients(order, "base", scanner);
 		restaurant.askAndSetIngredients(order, "protein", scanner);
 		restaurant.askAndSetIngredients(order, "topping", scanner);
 		restaurant.askAndSetIngredients(order, "topping", scanner);
 		restaurant.askAndSetIngredients(order, "topping", scanner);
-		
+		restaurant.askForTip(order, scanner);
 		order.printReceipt();
 	}
 		
