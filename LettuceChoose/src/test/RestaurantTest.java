01afda873c898aee4abc912a259e6950931be3b7
Jiwoo Seo <jiwooseo@wustl.edu>
2022-04-27 01:36:58 -0500

    updates
    bug fixes in restaurants.java and tests

--- LettuceChoose/src/test/RestaurantTest.java
+++ LettuceChoose/src/test/RestaurantTest.java
@@ -4,62 +4,78 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import Order.Order;
 import Ingredient.Ingredient;
 import Restaurant.Restaurant;;
 
 class RestaurantTest {
 
 	Restaurant testRestaurant;
+	Order testOrder;
 	
 	@BeforeEach
 	void setup() {
 		testRestaurant = new Restaurant();
+		testOrder = new Order(testRestaurant);
 	}
 	
 	@Test
 	void testDecrementQuantity() {
-		testRestaurant.decrementQuantity("salad");
+		int initialQuantity = testRestaurant.checkQuantity("salad");
+		testRestaurant.setOrder(testOrder, "base", "salad");
 		Ingredient selectedIngredient = testRestaurant.getInventory().get("salad");
 		int quantity = selectedIngredient.getQuantity();
-		assertEquals(2, quantity);
+		assertEquals(initialQuantity-1, quantity);
 	}
+	
 	
 	@Test
 	void testIncrementQuantity() {
+		int initialQuantity = testRestaurant.checkQuantity("salad");
 		testRestaurant.incrementQuantity("salad", 3);
 		Ingredient selectedIngredient = testRestaurant.getInventory().get("salad");
 		int quantity = selectedIngredient.getQuantity();
-		assertEquals(6, quantity);
+		assertEquals(initialQuantity+3, quantity);
 	}
 	
 	@Test
 	void testInStockWithExistingItem() {
-		assertEquals(testRestaurant.isInStock("salad"), true);
+		assertEquals(true, testRestaurant.isInStock("edamame"));
 		for(int i = 0; i <= 2; i++) {
-			testRestaurant.decrementQuantity("salad");
+			testRestaurant.setOrder(testOrder, "topping", "edamame");
 		}
-		assertEquals(testRestaurant.isInStock("salad"), false);
+		assertEquals(false, testRestaurant.isInStock("edamame"));
 	}
 	
+
+	
+	// checkQuantity() returns -1 if the input is an invalid ingredient
 	@Test
 	void testCheckQuantity() {
-		assertEquals(testRestaurant.checkQuantity("salad"), 3);
-		assertEquals(testRestaurant.checkQuantity("chicken"), 3);
-		assertEquals(testRestaurant.checkQuantity("mango"), 3);
-		assertEquals(testRestaurant.checkQuantity("saladdddd"), -1);
-		assertEquals(testRestaurant.checkQuantity("CHicken"), -1);
-		assertEquals(testRestaurant.checkQuantity("manGO"), -1);
+		assertEquals(3, testRestaurant.checkQuantity("salad"));
+		assertEquals(3, testRestaurant.checkQuantity("chicken"));
+		assertEquals(3, testRestaurant.checkQuantity("mango"));
+		assertEquals(-1, testRestaurant.checkQuantity("saladdddd"));
+		assertEquals(-1, testRestaurant.checkQuantity("CHicken"));
+		assertEquals(-1, testRestaurant.checkQuantity("manGO"));
 	}
 	
 	@Test
 	void testCheckQuantityWithDecrement() {
+		int initialQuantity = testRestaurant.checkQuantity("salad");
 		testRestaurant.decrementQuantity("salad");
-		assertEquals(testRestaurant.checkQuantity("salad"), 2);
+		assertEquals(initialQuantity-1, testRestaurant.checkQuantity("salad"));
 	}
 	
+	// If user cancels the order in the middle of the order, 
+	// the quantity of the base shouldn't be decremented
 	@Test
-	void testResetOrder() {
-		testRestaurant.decrementQuantity("salad");
+	void testCancelOrder() {
+		
+		int initialQuantity = testRestaurant.checkQuantity("salad");
+		testRestaurant.setOrder(testOrder, "base", "salad");
+		testRestaurant.resetOrder(testOrder);
+		assertEquals(initialQuantity, testRestaurant.checkQuantity("salad"));
 	}
 	
 	@Test
--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ javac Order/Order.java
 javac Ingredient/Ingredient.java
 javac Restaurant/Restaurant.java
 javac -encoding ISO-8859-1 main.java
+javac -encoding UTF-8 main.java
 javac main.java
 
 java main
